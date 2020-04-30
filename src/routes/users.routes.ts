c7ee1ca5f3247bf56543996bc79d32bb888c9261
ggotha <snakygamer@gmail.com>
2020-04-30 18:00:31 -0300

    feat: Create user route has been created

---
+++ src/routes/users.routes.ts
@@ -0,0 +1,28 @@
+import { Router } from "express";
+import { parseISO } from "date-fns";
+
+import AppointmentsRepository from "../repositories/AppointmentsRepository";
+import CreateAppointmentService from "../services/CreateAppointmentService";
+import { getCustomRepository } from "typeorm";
+
+import CreateUserService from "../services/CreateUserService";
+
+const usersRouter = Router();
+
+usersRouter.post("/", async (req, res) => {
+  try {
+    const { name, email, password } = req.body;
+
+    const createUser = new CreateUserService();
+
+    const user = await createUser.execute({ name, email, password });
+
+    delete user.password;
+
+    return res.json(user);
+  } catch (err) {
+    return res.status(400).json({ error: err.message });
+  }
+});
+
+export default usersRouter;
